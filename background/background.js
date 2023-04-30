6f9df65361c7f45f6f17e293f39465fc2321c62d
arinmis <mustafa_arinmis@outlook.com>
2023-04-30 21:39:37 +0300

    Hide home feed with checkbox

--- background/background.js
+++ background/background.js
@@ -1,13 +1,17 @@
 // default options for removing
 // videos and comment section
-let isHiddenRecomVideos = true;
-let isHiddenComments = true;
+const isHiddenRecomVideos = true;
+const isHiddenComments = true;
+const isHiddenHomeFeed = true;
 
 // initailize hiding state
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ isHiddenRecomVideos });
   chrome.storage.sync.set({ isHiddenComments });
+  chrome.storage.sync.set({ isHiddenHomeFeed });
+
   // logout defult values
   console.log(`isHiddenComments: ${isHiddenComments}`);
   console.log(`isHiddenRecomVideos: ${isHiddenRecomVideos}`);
+  console.log(`isHiddenRecomVideos: ${isHiddenHomeFeed}`);
 });
--- popup/popup.js
+++ popup/popup.js
@@ -1,21 +1,21 @@
 // Initialize button with user's preferred color
 const recomVideos = document.getElementById("recomVideos");
 const comments = document.getElementById("comments");
-console.log('popup.js is called');
-
+const homeFeed = document.getElementById("homeFeed");
+console.log("popup.js is called");
 
 // listen recommend checkbox
 window.addEventListener("load", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.storage.sync.get(
-    ["isHiddenRecomVideos", "isHiddenComments"],
-    ({ isHiddenRecomVideos, isHiddenComments }) => {
-      recomVideos.checked = isHiddenRecomVideos
-      comments.checked = isHiddenComments
+    ["isHiddenRecomVideos", "isHiddenComments", "isHiddenHomeFeed"],
+    ({ isHiddenRecomVideos, isHiddenComments, isHiddenHomeFeed }) => {
+      recomVideos.checked = isHiddenRecomVideos;
+      comments.checked = isHiddenComments;
+      homeFeed.checked = isHiddenHomeFeed;
     }
   );
-
 });
 
 // listen recommend checkbox
@@ -28,10 +28,8 @@ recomVideos.addEventListener("change", async () => {
   });
 });
 
-
-
 // listen recommend checkbox
-comments.addEventListener("change", async () => {
+homeFeed.addEventListener("change", async () => {
   let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
 
   chrome.scripting.executeScript({
@@ -40,6 +38,15 @@ comments.addEventListener("change", async () => {
   });
 });
 
+// listen home feed checkbox
+comments.addEventListener("change", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: hideHomeFeed,
+  });
+});
 
 /* hide recommendation element of youtube
  * current id is: 'secondary'
@@ -49,25 +56,21 @@ function hideRecomVideos() {
   chrome.storage.sync.get("isHiddenRecomVideos", ({ isHiddenRecomVideos }) => {
     // chage hide status
     console.log("here");
-    isHiddenRecomVideos = !isHiddenRecomVideos
+    isHiddenRecomVideos = !isHiddenRecomVideos;
     // update videos visibility data
-    chrome.storage.sync.set({"isHiddenRecomVideos": isHiddenRecomVideos});
-    // check if the url is correct 
-    if (!window.location.href.includes("youtube.com"))
-      return
+    chrome.storage.sync.set({ isHiddenRecomVideos: isHiddenRecomVideos });
+    // check if the url is correct
+    if (!window.location.href.includes("youtube.com")) return;
     // block recommendations
     if (isHiddenRecomVideos) {
-      document.getElementById('secondary-inner').style.display = 'none'
-      console.log('video is hiding');
-    }
-    else {
-      document.getElementById('secondary-inner').style.display = 'block'
-      console.log('video is showing');
+      document.getElementById("secondary-inner").style.display = "none";
+      console.log("video is hiding");
+    } else {
+      document.getElementById("secondary-inner").style.display = "block";
+      console.log("video is showing");
     }
   });
-
 }
-
 
 /*
  * hide comments element of youtube
@@ -77,21 +80,42 @@ function hideRecomVideos() {
 function hideComments() {
   chrome.storage.sync.get("isHiddenComments", ({ isHiddenComments }) => {
     // change hide status
-    isHiddenComments = !isHiddenComments
+    isHiddenComments = !isHiddenComments;
     // update comment visibility data
-    chrome.storage.sync.set({"isHiddenComments": isHiddenComments});
-    // check if the url is correct 
+    chrome.storage.sync.set({ isHiddenComments: isHiddenComments });
+    // check if the url is correct
     console.log(isHiddenComments);
-    if (!window.location.href.includes("youtube.com"))
-      return
+    if (!window.location.href.includes("youtube.com")) return;
     // block recommendations
     if (isHiddenComments) {
-      console.log("comments are hiding")
-      document.getElementById('comments').style.display = 'none'
+      console.log("comments are hiding");
+      document.getElementById("comments").style.display = "none";
+    } else {
+      console.log("comments are showing");
+      document.getElementById("comments").style.display = "block";
     }
-    else {
-      console.log("comments are showing")
-      document.getElementById('comments').style.display = 'block'
+  });
+}
+
+/*
+ * hide home feed
+ */
+function hideComments() {
+  chrome.storage.sync.get("isHiddenHomeFeed", ({ isHiddenHomeFeed }) => {
+    // change hide status
+    isHiddenHomeFeed = !isHiddenHomeFeed;
+    // update comment visibility data
+    chrome.storage.sync.set({ isHiddenHomeFeed: isHiddenHomeFeed });
+    // check if the url is correct
+    console.log(isHiddenHomeFeed);
+    if (!window.location.href.includes("youtube.com")) return;
+    // block recommendations
+    if (isHiddenHomeFeed) {
+      console.log("home feed is hiding");
+      document.getElementById("contents").style.display = "none";
+    } else {
+      console.log("home feed is showing");
+      document.getElementById("contents").style.display = "block";
     }
   });
 }
